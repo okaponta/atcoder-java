7649e287c4807f28df3d2b2014ef36e713ab0e37
KoheiOkamoto <koko72.flavor@gmail.com>
2020-01-10 20:19:41 +0900

    commit template for atcoder submission.

---
+++ atcoder/src/template/Main.java
@@ -0,0 +1,17 @@
+package template;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		print(0);
+		sc.close();
+	}
+
+	public static void print(Object obj) {
+		System.out.println(obj);
+	}
+
+}
